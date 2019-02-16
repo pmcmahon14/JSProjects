92a28fea07c6532b915a0bf5a32418be6eaf8c2c
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-15 17:20:52 -0800

    set up to change color upon red input

--- ColorFinder/script.js
+++ ColorFinder/script.js
@@ -1,8 +1,16 @@
-const red = [0,0,0];
+const rgb = [0,0,0];
 const green = [0,0,0];
 const blue = [0,0,0];
 
 //https://www.w3schools.com/colors/colors_names.asp
-const rgb = () => {
 
-};
+/*document.addEventListener("DOMElementLoaded", function() {
+    document.getElementById('number').click(testNumber);
+    document.getElementById('number').keypress(enterKey);
+});*/
+
+document.querySelector('#red').addEventListener("input", red);
+
+function red() {
+    console.log('red value changed');
+}
