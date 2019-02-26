0b7d84d8260e7a3c1a7f1a853ec6136892fa8c52
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-25 19:51:37 -0800

    added event listener, play audio for correct key, and set currentTime = 0 to play other keys while current is still on

--- Drums/script.js
+++ Drums/script.js
@@ -1,3 +1,6 @@
-document.addEventListener("click", function() {
-
+document.addEventListener("keydown", function(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 });
