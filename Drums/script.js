f875aa4b541df43fc7e1503a181b7eac0952df93
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-25 20:19:23 -0800

    moved script to html page, now works

--- Drums/script.js
+++ Drums/script.js
@@ -1,6 +1,16 @@
 document.addEventListener("keydown", function(event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"`);
-    if (!audio) return;
-    audio.currentTime = 0;
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"`);
+    if (!audio) return; //stops function if key not audio
+    audio.currentTime = 0; //allows user to play other keys while current key still playing
     audio.play();
+    key.classList.add('playing'); //adds animation by adding class already in CSS
 });
+
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
